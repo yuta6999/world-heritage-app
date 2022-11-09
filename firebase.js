3272e64f65a9fb069e88f5e99b8e084c3cd96c6f
yuta6999 <you@example.com>
2022-11-09 23:20:34 +0900

    firebase

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBqOoT4R1Xlv8biyE7Ut2BfmjNlztgFQ-M",
   authDomain: "world-heritage-appp.firebaseapp.com",
@@ -13,5 +11,9 @@ const firebaseConfig = {
   appId: "1:363825588991:web:d1c2d19163a34417d77aca"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth };
+export default db;
